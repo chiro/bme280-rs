d9a59d675f1cddf5219ef49f69be5417bc88f6c9
chiro <i@chir.jp>
2017-08-13 22:07:32 +0900

    Add the generated documentation to doc/.

---
+++ doc/bme280_rs/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["bme280",""]]});
---
+++ doc/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["bme280_rs"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"bme280_rs/bme280/enum.Mode.html\" title=\"enum bme280_rs::bme280::Mode\">Mode</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
